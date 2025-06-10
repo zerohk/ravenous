6c5172a706d5b306a5bafd0737c5c3f2cf372870
zerohk <forlovelygirlfyt@gmail.com>
2025-06-10 17:58:39 +0800

    完成搜索栏组件，添加了两个输入框，一个提交按钮

---
+++ src/SearchBar.jsx
@@ -0,0 +1,12 @@
+function SearchBar() {
+  return (
+    <form action="#">
+      <input className="restaurant" type="text" />
+      <input className="location" type="text" />
+      <input type="submit" />
+    </form>
+  );
+}
+
+
+export default SearchBar;
